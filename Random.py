0cc8573d4b67672ba04f637fe9f524164ca20e30
HMonique <moniquehl@outlook.com>
2024-10-11 11:56:58 -0400

    Commit

--- Random.py
+++ Random.py
@@ -1,1 +1,17 @@
 import random
+# print(random.random())
+
+# print(random.randint(1, 100))
+# print(random.randint(1, 100))
+
+# print(random.choice('computer'))
+# print(random.choice([12, 23, 45, 67, 65, 43]))
+# print(random.choice((12, 23, 45, 67, 65, 43)))
+
+# alist = [20, 40, 80, 100, 120]
+# sampled_list = random.sample(alist, 3)
+# print(sampled_list)
+
+num_list = random.sample(range(100), 5)
+print(num_list)
+      
